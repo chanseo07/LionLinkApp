92e47a8d355a23593ecfb2b206769c0967261a92
chanseo07 <baebrian07@gmail.com>
2017-02-06 11:15:51 -0500

    Created project

---
+++ Lionlinks/LionlinksTests/LionlinksTests.swift
@@ -0,0 +1,36 @@
+//
+//  LionlinksTests.swift
+//  LionlinksTests
+//
+//  Created by Brian Bae on 2017. 2. 6..
+//  Copyright © 2017년 Sakura Clicker. All rights reserved.
+//
+
+import XCTest
+@testable import Lionlinks
+
+class LionlinksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

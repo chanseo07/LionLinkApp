765bcb6e0ca1cb24dda52345ebe4336b86dce408
chanseo07 <baebrian07@gmail.com>
2017-02-08 08:29:43 -0500

    Initial design

---
+++ Lionlinks/LoginVC.swift
@@ -0,0 +1,41 @@
+//
+//  LoginVC.swift
+//  Lionlinks
+//
+//  Created by Brian Bae on 2017. 2. 7..
+//  Copyright © 2017년 Sakura Clicker. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBAction func LogIn(_ sender: Any) {
+        //Code for login
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
